68d090a5db895ea31bea760f15db26b3a595e0b9
JamieWilson2000 <jamiewilson2000@yahoo.com>
2021-10-26 23:38:28 +0000

    Add home app and templates

--- pound_dog/settings.py
+++ pound_dog/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +61,10 @@ ROOT_URLCONF = 'pound_dog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),  # adds templates to the directory path
+            os.path.join(BASE_DIR, 'templates', 'allauth'),  # adds allauth to the directory path
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pound_dog/urls.py
+++ pound_dog/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')), # This tells project to access the urls.py in the home directory
 ]
